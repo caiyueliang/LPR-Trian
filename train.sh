7bd5bca1ca46fa5e6e3933c6de0dbe9856172a08
caiyueliang <393900414@qq.com>
2018-09-26 15:33:30 +0800

    update

--- train.sh
+++ train.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-python main.py train -ti ../Data/car_recognition/train -tl ../Data/car_recognition/train/labels_normal.txt -vi ../Data/car_recognition/test -vl ../Data/car_recognition/test/labels_normal.txt -b 16 -img-size 164 48 -label-len 7 -n 100 -c checkpoints/'weights.{epoch:02d}-{val_loss:.4f}.h5' -pre checkpoints/weights.h5 -log log
+epoch=20
+branch=16
 
-# python main.py train -ti ../Data/car_recognition/train -tl ../Data/car_recognition/train/labels_green.txt -vi ../Data/car_recognition/test -vl ../Data/car_recognition/test/labels_green.txt -b 16 -img-size 164 48 -label-len 8 -n 10 -c checkpoints/weights.h5 -pre checkpoints/weights.h5 -log log
+python main.py train -ti ../Data/car_recognition/train -tl ../Data/car_recognition/train/labels_normal.txt -vi ../Data/car_recognition/test -vl ../Data/car_recognition/test/labels_normal.txt -b ${branch} -img-size 164 48 -label-len 7 -n ${epoch} -c checkpoints/weights_blue.h5 -pre checkpoints/weights_green.h5 -log log
+
+python main.py train -ti ../Data/car_recognition/train -tl ../Data/car_recognition/train/labels_green.txt -vi ../Data/car_recognition/test -vl ../Data/car_recognition/test/labels_green.txt -b ${branch} -img-size 164 48 -label-len 8 -n ${epoch} -c checkpoints/weights_green.h5 -pre checkpoints/weights_blue.h5 -log log
+
+python main.py train -ti ../Data/car_recognition/train -tl ../Data/car_recognition/train/labels_normal.txt -vi ../Data/car_recognition/test -vl ../Data/car_recognition/test/labels_normal.txt -b ${branch} -img-size 164 48 -label-len 7 -n ${epoch} -c checkpoints/weights_blue.h5 -pre checkpoints/weights_green.h5 -log log
+
+python main.py train -ti ../Data/car_recognition/train -tl ../Data/car_recognition/train/labels_green.txt -vi ../Data/car_recognition/test -vl ../Data/car_recognition/test/labels_green.txt -b ${branch} -img-size 164 48 -label-len 8 -n ${epoch} -c checkpoints/weights_green.h5 -pre checkpoints/weights_blue.h5 -log log
