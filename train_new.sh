9b5058619a82e604458988ee855c1036513127ff
caiyueliang <393900414@qq.com>
2018-11-06 16:50:40 +0800

    update

---
+++ train_new.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+epoch=100
+branch=1
+lr=0.001
+
+python train.py train -ti ../Data/car_recognition/train -tl ../Data/car_recognition/train/labels_normal.txt -vi ../Data/car_recognition/test -vl ../Data/car_recognition/test/labels_normal.txt -b ${branch} -img-size 164 48 -n ${epoch} -lr ${lr} -c checkpoints/weights_new.h5 -pre checkpoints/weights_new.h5 -log log
