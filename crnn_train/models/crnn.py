cf41bf78048f7c0f145202d670ce344e6b307d13
lijinchao2007 <lijinchao2007@163.com>
2018-12-14 11:17:41 +0800

    update

--- crnn_train/models/crnn.py
+++ crnn_train/models/crnn.py
@@ -1,3 +1,4 @@
+# encoding:utf-8
 import torch.nn as nn
 import utils
 
---
+++ crnn_train/models/gru.py
@@ -0,0 +1,168 @@
+# encoding:utf-8
+import torch.nn as nn
+import utils
+from keras.layers import *
+from keras.layers import Input, Activation, Conv2D, BatchNormalization, Lambda, MaxPooling2D, Dropout
+
+
+# 基于GRU车牌识别模型
+def model_seq_rec():
+    width, height, n_len, n_class = 164, 48, 7, NUM_CHARS + 1
+    rnn_size = 256
+    input_tensor = Input(name='the_input', shape=(164, 48, 3), dtype='float32')
+    x = input_tensor
+    base_conv = 32
+    # print('input_tensor', x.shape)
+
+    for i in range(3):
+        x = Conv2D(base_conv * (2 ** i), (3, 3))(x)
+        # print('Conv2D', x.shape)
+        x = BatchNormalization()(x)
+        x = Activation('relu')(x)
+        x = MaxPooling2D(pool_size=(2, 2))(x)
+        # print('MaxPooling2D', x.shape)          # [None, 18, 4, 128]
+
+    conv_shape = x.get_shape()
+    # print(conv_shape)
+    x = Reshape(target_shape=(int(conv_shape[1]), int(conv_shape[2] * conv_shape[3])))(x)
+    # print('Reshape', x.shape)                   # [None, 18, 512]
+    x = Dense(32)(x)
+    # print('Dense', x.shape)                     # [None, 18, 32]
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+
+    gru_1 = GRU(rnn_size, return_sequences=True, kernel_initializer='he_normal', name='gru1')(x)
+    # print('gru_1', gru_1.shape)                 # [None, None, 256]
+    gru_1b = GRU(rnn_size, return_sequences=True, go_backwards=True, kernel_initializer='he_normal', name='gru1_b')(x)
+    # print('gru_1b', gru_1b.shape)               # [None, None, 256]
+    gru1_merged = add([gru_1, gru_1b])
+    # print('gru1_merged', gru1_merged.shape)     # [None, None, 256]
+
+    gru_2 = GRU(rnn_size, return_sequences=True, kernel_initializer='he_normal', name='gru2')(gru1_merged)
+    # print('gru_2', gru_2.shape)                 # [None, None, 256]
+    gru_2b = GRU(rnn_size, return_sequences=True, go_backwards=True, kernel_initializer='he_normal', name='gru2_b')(gru1_merged)
+    # print('gru_2b', gru_2b.shape)               # [None, None, 256]
+    x = concatenate([gru_2, gru_2b])
+    # print('concatenate', x.shape)               # [None, None, 512]
+    x = Dropout(0.25)(x)
+    x = Dense(n_class, kernel_initializer='he_normal', activation='softmax')(x)
+    # print('Dense', x.shape)                     # [None, 18, 84]
+
+    y_pred = x
+    return input_tensor, y_pred
+
+
+class MyConv2D(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        self.conv = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=(3, 3))
+        self.bn = nn.BatchNorm2d()
+        self.relu = nn.ReLU()
+        self.max_pool = nn.MaxPool2d(kernel_size=(2, 2))
+
+    def forward(self, input):
+        x = self.conv(input)
+        x = self.bn(x)
+        x = self.relu(x)
+        output = self.max_pool(x)
+        return output
+
+
+class CGRU(nn.Module):
+    def __init__(self, width, height, n_class, n_c=3):
+        super(CGRU, self).__init__()
+        assert height % 16 == 0, 'imgH has to be a multiple of 16'
+        n_base_conv = 32
+
+        self.conv_1 = MyConv2D(in_channels=n_base_conv, out_channels=n_base_conv)
+        self.conv_2 = MyConv2D(in_channels=n_base_conv, out_channels=n_base_conv * 2)
+        self.conv_3 = MyConv2D(in_channels=n_base_conv * 2, out_channels=n_base_conv * 4)
+
+        self.cnn =
+        self.gru =
+
+    def forward(self, input):
+        return output
+
+# class BidirectionalLSTM(nn.Module):
+#
+#     def __init__(self, nIn, nHidden, nOut, ngpu):
+#         super(BidirectionalLSTM, self).__init__()
+#         self.ngpu = ngpu
+#
+#         self.rnn = nn.LSTM(nIn, nHidden, bidirectional=True)
+#         self.embedding = nn.Linear(nHidden * 2, nOut)
+#
+#     def forward(self, input):
+#         recurrent, _ = utils.data_parallel(
+#             self.rnn, input, self.ngpu)  # [T, b, h * 2]
+#
+#         T, b, h = recurrent.size()
+#         t_rec = recurrent.view(T * b, h)
+#         output = utils.data_parallel(
+#             self.embedding, t_rec, self.ngpu)  # [T * b, nOut]
+#         output = output.view(T, b, -1)
+#
+#         return output
+
+
+class CRNN(nn.Module):
+
+    def __init__(self, imgH, nc, nclass, nh, ngpu, n_rnn=2, leakyRelu=False):
+        super(CRNN, self).__init__()
+        self.ngpu = ngpu
+        assert imgH % 16 == 0, 'imgH has to be a multiple of 16'
+
+        ks = [3, 3, 3, 3, 3, 3, 2]
+        ps = [1, 1, 1, 1, 1, 1, 0]
+        ss = [1, 1, 1, 1, 1, 1, 1]
+        nm = [64, 128, 256, 256, 512, 512, 512]
+
+        cnn = nn.Sequential()
+
+        def convRelu(i, batchNormalization=False):
+            nIn = nc if i == 0 else nm[i - 1]
+            nOut = nm[i]
+            cnn.add_module('conv{0}'.format(i),
+                           nn.Conv2d(nIn, nOut, ks[i], ss[i], ps[i]))
+            if batchNormalization:
+                cnn.add_module('batchnorm{0}'.format(i), nn.BatchNorm2d(nOut))
+            if leakyRelu:
+                cnn.add_module('relu{0}'.format(i),
+                               nn.LeakyReLU(0.2, inplace=True))
+            else:
+                cnn.add_module('relu{0}'.format(i), nn.ReLU(True))
+
+        convRelu(0)
+        cnn.add_module('pooling{0}'.format(0), nn.MaxPool2d(2, 2))  # 64x16x64
+        convRelu(1)
+        cnn.add_module('pooling{0}'.format(1), nn.MaxPool2d(2, 2))  # 128x8x32
+        convRelu(2, True)
+        convRelu(3)
+        cnn.add_module('pooling{0}'.format(2), nn.MaxPool2d((2, 2),
+                                                            (2, 1),
+                                                            (0, 1)))  # 256x4x16
+        convRelu(4, True)
+        convRelu(5)
+        cnn.add_module('pooling{0}'.format(3), nn.MaxPool2d((2, 2),
+                                                            (2, 1),
+                                                            (0, 1)))  # 512x2x16
+        convRelu(6, True)  # 512x1x16
+
+        self.cnn = cnn
+        self.rnn = nn.Sequential(
+            BidirectionalLSTM(512, nh, nh, ngpu),
+            BidirectionalLSTM(nh, nh, nclass, ngpu)
+        )
+
+    def forward(self, input):
+        # conv features
+        conv = utils.data_parallel(self.cnn, input, self.ngpu)
+        b, c, h, w = conv.size()
+        assert h == 1, "the height of conv must be 1"
+        conv = conv.squeeze(2)
+        conv = conv.permute(2, 0, 1)  # [w, b, c]
+
+        # rnn features
+        output = utils.data_parallel(self.rnn, conv, self.ngpu)
+
+        return output
