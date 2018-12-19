5349ed16979fcbce67f2512da09317d6bcc06714
lijinchao2007 <lijinchao2007@163.com>
2018-12-19 17:48:54 +0800

    update

--- crnn_train/models/crnn.py
+++ crnn_train/models/crnn.py
@@ -44,30 +44,28 @@ class CRNN(nn.Module):
         def convRelu(i, batchNormalization=False):
             nIn = nc if i == 0 else nm[i - 1]
             nOut = nm[i]
-            cnn.add_module('conv{0}'.format(i),
-                           nn.Conv2d(nIn, nOut, ks[i], ss[i], ps[i]))
+            cnn.add_module('conv{0}'.format(i), nn.Conv2d(nIn, nOut, ks[i], ss[i], ps[i]))
             if batchNormalization:
                 cnn.add_module('batchnorm{0}'.format(i), nn.BatchNorm2d(nOut))
             if leakyRelu:
-                cnn.add_module('relu{0}'.format(i),
-                               nn.LeakyReLU(0.2, inplace=True))
+                cnn.add_module('relu{0}'.format(i), nn.LeakyReLU(0.2, inplace=True))
             else:
                 cnn.add_module('relu{0}'.format(i), nn.ReLU(True))
 
         convRelu(0)
-        cnn.add_module('pooling{0}'.format(0), nn.MaxPool2d(2, 2))  # 64x16x64
+        cnn.add_module('pooling{0}'.format(0), nn.MaxPool2d(2, 2))      # 64x16x64
         convRelu(1)
-        cnn.add_module('pooling{0}'.format(1), nn.MaxPool2d(2, 2))  # 128x8x32
+        cnn.add_module('pooling{0}'.format(1), nn.MaxPool2d(2, 2))      # 128x8x32
         convRelu(2, True)
         convRelu(3)
         cnn.add_module('pooling{0}'.format(2), nn.MaxPool2d((2, 2),
                                                             (2, 1),
-                                                            (0, 1)))  # 256x4x16
+                                                            (0, 1)))    # 256x4x16
         convRelu(4, True)
         convRelu(5)
         cnn.add_module('pooling{0}'.format(3), nn.MaxPool2d((2, 2),
                                                             (2, 1),
-                                                            (0, 1)))  # 512x2x16
+                                                            (0, 1)))    # 512x2x16
         convRelu(6, True)  # 512x1x16
 
         self.cnn = cnn
